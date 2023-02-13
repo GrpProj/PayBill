ecc20c325f98ed1edca3a97f9ddb62bffe5fc952
Jonathan <jonathandabre01@gmail.com>
2023-02-14 00:41:38 +0530

    added slider homepage

--- home.js
+++ home.js
@@ -51,3 +51,33 @@ if (navigator.geolocation) {
     alert("Geolocation is not supported by this browser.");
   }
   
+
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
